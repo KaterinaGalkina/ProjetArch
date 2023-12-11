dfb25f85d8251a9599add8109d4191eaf3d9450d
Katerina <katiegodoonova@gmail.com>
2023-12-11 22:35:49 +0100

    Partial completion of traduction code

--- translate.c
+++ translate.c
@@ -8,14 +8,14 @@
 #define NbColons 200
 #define NbLines 1000
 
-long decToBin(int val) {
+long long int decToBin(int val) {
     // Fonction pour traduire un nombre decimal en binaire
-    long binarynum = 0;
-    int rem, temp = 1;
+    long long int binarynum = 0;
+    long long int rem, temp = 1;
 
-    while (val!=0)
+    while (val != 0)
     {
-        rem = val%2;
+        rem = val % 2;
         val = val / 2;
         binarynum = binarynum + rem*temp;
         temp = temp * 10;
@@ -26,16 +26,23 @@ long decToBin(int val) {
 int tagCollect(int index, char actualLine[], char allTags[NbLines][NbColons]) {
     // Fonction qui parcourt la ligne actuelle et stocke les etiquettes trouves dans le tableau "allTags"
     int tagPres=0;
-    for (int i=0; i<strlen(actualLine); i++) {
-        if (actualLine[i]==':') {
+    for (int i = 0; i < strlen(actualLine); i++) {
+        if (actualLine[i] == ':') {
             tagPres = 1;
         }
     }
     int ind = 0;
     if (tagPres) {
-        while (actualLine[ind]!=':') {
-            allTags[index][ind]=actualLine[ind];
-            ind++;
+        while (actualLine[ind] != ':') {
+            int carVal = actualLine[ind];
+            if (carVal < 48 || (carVal > 57 && carVal < 65) || (carVal > 90 && carVal < 97) || carVal > 122){
+                printf("Error : le format de l'etiquette n'est pas valide le caractere %c n'est pas accepte\n", carVal);
+                return 0;
+            }
+            else {
+                allTags[index][ind] = actualLine[ind];
+                ind++;
+            }
         }
         ind++;
         return 1;
@@ -45,92 +52,121 @@ int tagCollect(int index, char actualLine[], char allTags[NbLines][NbColons]) {
 
 void traduction(int index, char actualLine[], char allTags[NbLines][NbColons], int presTag, char translatedLine[NbColons]) {
     char* codeOpVals[32] = {"add", "sub", "mul", "div", "and", "or", "xor", "shl", "", "", "ldb", "ldw", "stb", "stw", "", "", "", "", "","", "jmp", "jzs", "jzc", "jcs", "jcc", "jns", "jnc", "in", "out", "rnd", "", "hlt"}; // Collection des commande du code opératoire
-    int ind = 0; // Indice principale avec laquelle on parcourt la ligne une fois
+    long translatedLineBin[5]={0,0,0,0,0};
+    int ind = 0; // Indice principal avec lequel on parcourt la ligne une fois
     if (presTag) { // On parcourt la ligne actuelle jusque la fin de l'etiquette
-        while (actualLine[ind]!=':') {
+        while (actualLine[ind] != ':') {
             ind++;
         }
         ind++;
     }
-    while (actualLine[ind]==' ') { // On parcourt la ligne actuelle jusqu'a ce qu'on tombe sur une commande
+    while (actualLine[ind] == ' '|| actualLine[ind] == '0' || actualLine[ind] == '1') { // On parcourt la ligne actuelle jusqu'a ce qu'on tombe sur une commande
         ind++;
     }
-    char command[10]=""; // Chaine de caractere qui contient la commande de la ligne actuelle
-    int j=0; // Indice auxiliere pour stocker lettre par lettre la commande actuelle
-    while (actualLine[ind]!=' ' && actualLine[ind]!='\0' && actualLine[ind]!='\n') { // stockage de la commande actuelle
-        command[j]=actualLine[ind];
+    char command[10] = ""; // Chaine de caractere qui contient la commande de la ligne actuelle
+    int j = 0; // Indice auxiliaire pour stocker lettre par lettre la commande actuelle
+    while (actualLine[ind] != ' ' && actualLine[ind] != '\0' && actualLine[ind] != '\n') { // stockage de la commande actuelle
+        command[j] = actualLine[ind];
         ind++;
         j++;
     }
     
-    int i=0; // indice qui va contenir le numero de la commande
-    while (i<32 && strcmp(codeOpVals[i], command)) { // la recherche du numero de la commande dans la Collection des commande du code opératoire
+    int i = 0; // indice qui va contenir le numero de la commande
+    while (i < 32 && strcmp(codeOpVals[i], command)) { // la recherche du numero de la commande dans la Collection des commande du code opératoire
         i++;
     }
     
-    long binNb = 0; // Variable temporaire pour stocker l'adresse en binaire des commandes ou parametre
+    long long int binNb = 0; // Variable temporaire pour stocker l'adresse en binaire des commandes ou parametre
+    
     
     if (i<32) { // Si la commande existe et est trouve on la traduit en binaire
         binNb = decToBin(i);
         printf("Command : %s\n", command);
-        printf(" trad : %ld\n", binNb);
+        printf(" trad : %lld \n", binNb);
+        translatedLineBin[0] = i;
+        
     } else {
         printf("Error : commande %s n'existe pas\n", command);
     }
     
     
-    char parameters[3][4] = {"","",""}; // Tableau de caracteres qui contient tous les arguments de la ligne actuelle
+    char parameters[3][15] = {"","",""}; // Tableau de caracteres qui contient tous les arguments de la ligne actuelle
     
-    while (actualLine[ind]==' ') {
+    while (actualLine[ind] == ' ') {
         ind++;
     }
     
-    int col=0; // Indice pour stocker le numero de l'argument qu'on est en train de parcourir
-    while (actualLine[ind]!='\n' && actualLine[ind]!='\0') { // On stocker tous les argument de la ligne actuelle
+    int col = 0; // Indice pour stocker le numero de l'argument qu'on est en train de parcourir
+    while (actualLine[ind] != '\n' && actualLine[ind] != '\0') { // On stocker tous les argument de la ligne actuelle
         // !!! Gerer le cas ou c'est une valeur immédiate !!!
-        j=0; // Indice auxiliere qui aide a stocker lettre par lettre un parametre qu'on est en train de parcourir
-        while (actualLine[ind]!=',' && actualLine[ind]!='\n' && actualLine[ind]!='\0') {
-            parameters[col][j]=actualLine[ind];
+        j = 0; // Indice auxiliaire qui aide a stocker lettre par lettre un parametre qu'on est en train de parcourir
+        while (actualLine[ind] != ',' && actualLine[ind] != '\n' && actualLine[ind] != '\0') {
+            parameters[col][j] = actualLine[ind];
             ind++;
             j++;
         }
-        if (actualLine[ind]!='\n' && actualLine[ind]!='\0') {
-            ind+=2;
+        if (actualLine[ind] != '\n' && actualLine[ind] != '\0') {
+            ind += 2;
         }
         col++;
     }
     // "col" contient le nombre d'arguments
-    j=0; // Le numero d'argument qu'on est en train de parcourir (j<col)
-    int y; // Indice auxiliere pour comparaison des lettres
-    int parNb=0; // Variable auxiliere pour recuperer la valeur decimale du numero du parametre ou de l'adrese ou pointe l'etiquette
-    int nbAux; // Variable auxiliere qui aide a cacluler le numero du registre
-    while (j<col) {
+    j = 0; // Le numero d'argument qu'on est en train de parcourir (j<col)
+
+    while (j < col) {
         printf(" parametre : %s\n", parameters[j]);
-        parNb=0;
-        if (parameters[j][0]=='r') { // Si l'argument est le registre, on traduit son numero en binaire
-            // !!! Gerer le cas ou l'etiquette est sous forme "r..." !!!
-            y=1;
-            while ((parameters[j][y]!='\0')) {
-                nbAux = (parameters[j][y]-48)*pow(10, strlen(parameters[j])-1-y);
+        int y; // Indice auxiliaire pour comparaison des lettres
+        int parNb = 0; // Variable auxiliaire pour recuperer la valeur decimale du numero du parametre ou de l'adrese ou pointe l'etiquette
+        int nbAux; // Variable auxiliaire qui aide a calculer la valeur immediate
+        int immPres = 0; // variable indiquant si le parametre est une valeur immediate ou non
+        if (parameters[j][0] == '#'){
+            if (j!=2) {
+                printf("Error : valeur immediate doit etre passe en 2e source \n");
+            }
+            immPres = 1;
+            y = 1;
+            while ((parameters[j][y] != '\0')) {
+                nbAux = (parameters[j][y] - 48) * pow(10, strlen(parameters[j])- 1 - y);    // convertir le caractere ascii en entier (0 car = 48 en decimal)
                 parNb += nbAux;
                 y++;
             }
-            binNb = decToBin(parNb); // Convertion du numero de registre de decimal en binaire
-            if (binNb<0 || binNb>11111) {
+            printf(" immediate value translated : %d\n", parNb);
+            translatedLineBin[3] = 1;
+            translatedLineBin[4] = parNb;
+            binNb = decToBin(parNb);
+            // conversion en binaire ne marche pas
+            printf(" immediate value translated : %lld\n", binNb);
+        }
+        else if (parameters[j][0]=='r') { // Si l'argument est le registre, on traduit son numero en binaire
+            // !!! Gerer le cas ou l'etiquette est sous forme "r..." !!!
+            y = 1;
+            while ((parameters[j][y]!='\0')) {
+                nbAux = (parameters[j][y]-48)*pow(10, strlen(parameters[j])-1-y);    // convertir le caractere ascii en entier (0 car = 48 en decimal)
+                parNb += nbAux;
+                y++;
+            }
+            if (j!=2) {
+                translatedLineBin[j+1] = parNb;
+            } else {
+                translatedLineBin[3] = 0;
+                translatedLineBin[4] = parNb;
+            }
+            binNb = decToBin(parNb); // Conversion du numero de registre de decimal en binaire
+            if (binNb < 0 || binNb > 11111) {
                 printf("Error : le registre %s n'existe pas\n", parameters[j]);
             } else {
-                printf(" parametre translated : %ld\n", binNb);
+                printf(" parametre translated : %lld\n", binNb);
             }
             
         } else { // Si l'argument est une etiquette, on cherche l'adresse ou elle pointe
-            i=0; // Indice auxiliere qui va indiquer sur l'indice de l'etiquette dans le tableau des etiquettes
+            i = 0; // Indice auxiliaire qui va indiquer sur l'indice de l'etiquette dans le tableau des etiquettes
             int equal = 1; // Variable qui =0 si on a trouve l'indice de l'etiquette dans le tableau des etiquettes
-            while (i<NbLines && equal) { // On cherche l'indice de l'etiquette dans le tableau des etiquettes
-                y=0;
+            while (i < NbLines && equal) { // On cherche l'indice de l'etiquette dans le tableau des etiquettes
+                y = 0;
                 while (allTags[i][y]!='\0' && parameters[j][y]!='\0' && allTags[i][y]==parameters[j][y]) {
                     y++;
                 }
-                if (y==strlen(parameters[j]) && y==strlen(allTags[i])) {
+                if (y == strlen(parameters[j]) && y == strlen(allTags[i])) {
                     equal = 0;
                     parNb = i*4; // On a trouve l'indice de l'etiquette et comme chaque instruction fait 32 bits, soit 4 octets, on multiplie l'indice par 4
                 }
@@ -140,11 +176,21 @@ void traduction(int index, char actualLine[], char allTags[NbLines][NbColons], i
                 printf("Error : tag %s n'existe pas\n", parameters[j]);
             } else {
                 binNb = decToBin(parNb);
-                printf(" tag translated : %ld\n", binNb);
+                translatedLineBin[3] = 0;
+                translatedLineBin[4] = parNb;
+                printf(" tag translated : %lld\n", binNb);
             }
         }
         j++;
     }
+    int nbAux2 = (translatedLineBin[0]<<27) + (translatedLineBin[1]<<22) + (translatedLineBin[2]<<17) + (translatedLineBin[3]<<16) + translatedLineBin[4];
+    printf("Traduction : %x\n", nbAux2);
+    
+    int indxx = 0;
+    while (indxx < 5) {
+        printf("%ld\n", translatedLineBin[indxx]);
+        indxx++;
+   }
 }
 
 int main(int argc, char* argv[]) {
@@ -157,17 +203,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     
-    int i = 0; // Indice qui aide a remplir le tableau "TabLines" et des que le fichier est parcouru va indiquer le nombre de ligne dans le fichier source
+    int nbLines = 0; // Indice qui aide a remplir le tableau "TabLines" et des que le fichier est parcouru va indiquer le nombre de ligne dans le fichier source
     int tagsPresence; // Variable qui indique la presence d'une nouvelle etiquette sur une ligne ("ici : ..")
     while (fgets(line, NbColons,fichier_orig)!=NULL) { // On parcourt le fichier pour trouver tous les etiquettes
-        TabLines[i] = strdup(line);
-        tagsPresence = tagCollect(i, TabLines[i], allTags);
-        i++;
+        TabLines[nbLines] = strdup(line);
+        tagsPresence = tagCollect(nbLines, TabLines[nbLines], allTags);
+        nbLines++;
     }
     // Verification que tous les etiquettes sont bien stockes
     printf("Tags : ");
     int ind = 0;
-    while (ind<i) {
+    while (ind < nbLines) {
         printf("%s\n", allTags[ind]);
         ind++;
     }
@@ -183,10 +229,10 @@ int main(int argc, char* argv[]) {
     
     char translatedLine[NbColons]; // Nouvelle ligne qui est traduit
     int newI = 0; // Indice avec la quelle on va parcourir le tableau "TabLines", qui va indiquer la ligne ou on se trouve
-    while (newI<i) {
+    while (newI < nbLines) {
         tagsPresence = tagCollect(newI, TabLines[newI], allTags); // On verifie si la ligne contient une initialisation d'une etiquette
         traduction(newI, TabLines[newI], allTags, tagsPresence, translatedLine);
-        //fprintf(fichierTranlated, "%s\n", TabLines[i]);
+        //fprintf(fichierTranlated, "%s\n", TabLines[nbLines]);
         newI++;
     }
 
@@ -194,4 +240,8 @@ int main(int argc, char* argv[]) {
     
     return 0;
 }
+
+
+
+
 
